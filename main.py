e7cbcf766236b07c98b0c6e61c4503bd550eeee5
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-25 17:38:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+a=int(input())
+b=int(input())
+c=int(input())
+number_1=(a>9) and (a<21)
+number_2=(b>9) and (b<21)
+number_3=(c>9) and (c<21)
+between=number_1 or number_2 or number_3
+print(between)
